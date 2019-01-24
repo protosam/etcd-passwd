19e4e4d089fcca1d2d4fae7c51e00a8bbbd353e2
Sam <samigknighted@gmail.com>
2019-01-24 16:02:09 -0500

    Update shadow.go

--- shadow.go
+++ shadow.go
@@ -49,15 +49,15 @@ func go_getspnam_r(name string, spwd *C.struct_spwd, buf *C.char, buflen C.size_
 
 /*
    struct spwd {
-		 char          *sp_namp; /* user login name */
-		 char          *sp_pwdp; /* encrypted password */
-		 long int      sp_lstchg; /* last password change */
-		 long int      sp_min; /* days until change allowed. */
-		 long int      sp_max; /* days before change required */
-		 long int      sp_warn; /* days warning for expiration */
-		 long int      sp_inact; /* days before account inactive */
-		 long int      sp_expire; /* date when account expires */
-		 unsigned long int  sp_flag; /* reserved for future use */
+		 char          *sp_namp; // user login name 
+		 char          *sp_pwdp; // encrypted password 
+		 long int      sp_lstchg; // last password change 
+		 long int      sp_min; // days until change allowed. 
+		 long int      sp_max; // days before change required 
+		 long int      sp_warn; // days warning for expiration 
+		 long int      sp_inact; // days before account inactive 
+		 long int      sp_expire; // date when account expires 
+		 unsigned long int  sp_flag; // reserved for future use 
    }
 */
 
