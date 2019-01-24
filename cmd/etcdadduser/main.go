299ce60d4853166a072b6b4336ba2a09653c4692
Sam <samigknighted@gmail.com>
2019-01-24 16:36:28 -0500

    Update main.go

--- cmd/etcdadduser/main.go
+++ cmd/etcdadduser/main.go
@@ -7,7 +7,12 @@ import (
 	"os"
 	"path/filepath"
 
-	etcdsshd "github.com/protosam/etcd-passwd"
+	"github.com/protosam/etcd-passwd"
+
+	"github.com/kless/osutil/user/crypt/sha512_crypt"
+	"crypto/rand"
+	"encoding/hex"
+
 )
 
 var (
@@ -15,6 +20,7 @@ var (
 	flagEtcdPrefix = flag.String("etcd-prefix", "/etcd-passwd", "etcd prefix")
 
 	flagName  = flag.String("name", "", "Name")
+	flagPassword  = flag.String("password", "!!", "Password")
 	flagUID   = flag.Int("uid", -1, "UID")
 	flagGID   = flag.Int("gid", -1, "GID")
 	flagGecos = flag.String("gecos", "", "Gecos")
@@ -37,7 +43,14 @@ func run() error {
 		home = *flagDir
 	}
 
-	return etcdsshd.AddUser(&etcdsshd.Passwd{*flagName, "!", etcdsshd.UID(*flagUID), etcdsshd.GID(*flagGID), *flagGecos, home, *flagShell})
+	var password string 
+	if *flagPassword == "!!" || *flagPassword == "" {
+		password = "!!"
+	}else{
+		password = shadow_word(*flagPassword)
+	}
+	
+	return etcdpasswd.AddUser(&etcdpasswd.Passwd{*flagName, password, etcdpasswd.UID(*flagUID), etcdpasswd.GID(*flagGID), *flagGecos, home, *flagShell})
 }
 
 func main() {
@@ -48,4 +61,21 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+
+
+func shadow_word(password string) (string, error) {
+	b := make([]byte, 12)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+
+	shadow_word := hex.EncodeToString(b)
+
+	c := sha512_crypt.New()
+	hash, err := c.Generate([]byte(password), []byte("$6$" + shadow_word))
+
+	return hash, err
 }
